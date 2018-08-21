b308f8bebf3e948de8a3dad69fc3aaefdfbbc395
Deseteral <deseteral@gmail.com>
2018-08-22 00:44:33 +0200

    Source maps for development builds

--- tools/build.js
+++ tools/build.js
@@ -2,24 +2,33 @@ const fs = require('fs');
 const babel = require('@babel/core');
 const rimraf = require('rimraf');
 
+const dev = process.argv[2] === '--dev';
+
 rimraf.sync('./dist');
 fs.mkdirSync('./dist');
 
 const inputHtml = fs.readFileSync('./src/index.html', { encoding: 'utf8' });
 
-const options = {
-  presets: ['minify'],
-  plugins: ['@babel/plugin-syntax-object-rest-spread'],
-};
+function writeFiles(htmlFileName, isDev) {
+  const options = {
+    sourceMaps: isDev ? 'inline' : false,
+    presets: ['minify'],
+    plugins: ['@babel/plugin-syntax-object-rest-spread'],
+  };
 
-const { code } = babel.transformFileSync('./src/game.js', options);
+  const { code } = babel.transformFileSync('./src/game.js', options);
 
-const outputHtml = [
-  inputHtml.split('\n').filter(l => l.length > 0),
-  `<script>${code}</script>`,
-].join('');
+  const outputHtml = [
+    inputHtml.split('\n').filter(l => l.length > 0),
+    isDev ? '<script src="game.js"></script>' : `<script>${code}</script>`,
+  ].join('');
 
-fs.writeFileSync('./dist/index.html', outputHtml, { encoding: 'utf8' });
+  fs.writeFileSync(`./dist/${htmlFileName}.html`, outputHtml, { encoding: 'utf8' });
+  if (isDev) fs.writeFileSync('./dist/game.js', code, { encoding: 'utf8' });
+}
+
+writeFiles('index', false);
+if (dev) writeFiles('index-dev', dev);
 
 const { size } = fs.statSync('./dist/index.html');
 const percent = ((size / 31337) * 100).toFixed(2);
