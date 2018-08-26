6f1e4cb74a0e7ba24b3b98434748a48176a2587f
Deseteral <deseteral@gmail.com>
2018-08-26 15:13:44 +0200

    Project now builds prod and dev simultaneously

--- tools/build.js
+++ tools/build.js
@@ -2,15 +2,18 @@ const fs = require('fs');
 const babel = require('@babel/core');
 const rimraf = require('rimraf');
 
-const dev = process.argv[2] === '--dev';
-const distDir = dev ? 'dev' : 'dist';
+const DEV_DIR = './dev';
+const DIST_DIR = './dist';
 
-rimraf.sync(`./${distDir}`);
-fs.mkdirSync(`./${distDir}`);
+rimraf.sync(DEV_DIR);
+rimraf.sync(DIST_DIR);
+
+fs.mkdirSync(DEV_DIR);
+fs.mkdirSync(DIST_DIR);
 
 const inputHtml = fs.readFileSync('./src/index.html', { encoding: 'utf8' });
 
-function writeFiles(htmlFileName, isDev) {
+function writeFiles(isDev) {
   const options = {
     sourceMaps: isDev ? 'inline' : false,
     presets: ['minify'],
@@ -24,13 +27,15 @@ function writeFiles(htmlFileName, isDev) {
     isDev ? '<script src="game.js"></script>' : `<script>${code}</script>`,
   ].join('');
 
-  fs.writeFileSync(`./${distDir}/${htmlFileName}.htm`, outputHtml, { encoding: 'utf8' });
-  if (isDev) fs.writeFileSync(`./${distDir}/game.js`, code, { encoding: 'utf8' });
+  const distDir = isDev ? DEV_DIR : DIST_DIR;
+
+  fs.writeFileSync(`${distDir}/index.htm`, outputHtml, { encoding: 'utf8' });
+  if (isDev) fs.writeFileSync(`${distDir}/game.js`, code, { encoding: 'utf8' });
 }
 
-writeFiles('index', false);
-if (dev) writeFiles('index-dev', dev);
+writeFiles(false);
+writeFiles(true);
 
-const { size } = fs.statSync(`./${distDir}/index.htm`);
+const { size } = fs.statSync(`${DIST_DIR}/index.htm`);
 const percent = ((size / 31337) * 100).toFixed(2);
 console.log(`${size} bytes, ${percent}%`);
