44707718c4b8eec70a302fd8a022a8cf4075861a
Deseteral <deseteral@gmail.com>
2018-08-26 15:31:05 +0200

    Removed testing message

--- README.md
+++
@@ -1,3 +0,0 @@
-# Shelter Rampage
-
-My game for the [Gynvael's Summer GameDev Challenge 2018](https://gynvael.coldwind.pl/?id=686).
--- tools/upload.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "Uploading 'dist' directory to GitHub Pages..."
-npm run build
-git subtree push --prefix dist origin gh-pages
